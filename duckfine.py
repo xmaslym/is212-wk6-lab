e00eb24697b6943599eddac7e792ef1081d6c20f
LAU Yi Meng <ymlau@smu.edu.sg>
2026-09-22 01:00:53 +0800

    update

--- duckfine.py
+++ duckfine.py
@@ -15,6 +15,6 @@ class DuckFine:
         fee = chargeable * self.DAILY_FEE
         if deluxe:
             fee *= 2
-        #fee = min(fee, self.MAX_FEE)
+        fee = min(fee, self.MAX_FEE)
         self.total_owed += fee
         return fee
